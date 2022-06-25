6e674f642910f8f24f26fe6b8498026c12f1c330
pooja anjana <pooja.anjana@kadellabs.com>
2022-06-25 13:04:41 +0530

    commit

---
+++ Desktop/new/home.js
@@ -0,0 +1,14 @@
+<section class="collection-container">
+    <a href="#" class="collection">
+        <img src="img/women-collection.png" alt="">
+        <p class="collection-title">women <br> apparels</p>
+    </a>
+    <a href="#" class="collection">
+        <img src="img/men-collection.png" alt="">
+        <p class="collection-title">men <br> apparels</p>
+    </a>
+    <a href="#" class="collection">
+        <img src="img/accessories-collection.png" alt="">
+        <p class="collection-title">accessories</p>
+    </a>
+</section>
